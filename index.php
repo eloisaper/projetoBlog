6f28e26f728cb559209b04437ce2445265d306c3
eloisaper <eloisaperuzzo2000@outlook.com>
2023-10-03 11:55:19 -0300

    10:00

--- index.php
+++ index.php
@@ -50,7 +50,7 @@
                             'AND',
                             'titulo',
                             'like',
-                            "%{busca}%"
+                            "%{$busca}%"
                         ];
                     }
 
